e701ddab7a3a53fc163c57ea2e56cb5019a59f2a
dunno <116356493+dunnotoru@users.noreply.github.com>
2024-02-08 19:05:01 +0700

    update: add icons to toolbar
    set localization template.
    add styles

--- IDE/MainWindow.xaml.cs
+++ IDE/MainWindow.xaml.cs
@@ -9,43 +9,6 @@ namespace IDE
         public MainWindow()
         {
             InitializeComponent();
-
-            App.LanguageChanged += App_LanguageChanged;
-
-            CultureInfo currentLanguage = App.Language;
-
-            lang.Items.Clear();
-            foreach (var item in App.Languages)
-            {
-                MenuItem langItem = new MenuItem();
-                langItem.Header = item.DisplayName;
-                langItem.Tag = item;
-                langItem.IsChecked = item.Equals(currentLanguage);
-                langItem.Click += LangItem_Click;
-                lang.Items.Add(langItem);
-            }
-        }
-
-        private void LangItem_Click(object sender, RoutedEventArgs e)
-        {
-            MenuItem mi = sender as MenuItem;
-            if (mi == null) return;
-
-            CultureInfo lang = mi.Tag as CultureInfo;
-            if (lang == null) return;
-
-            App.Language = lang;
-        }
-
-        private void App_LanguageChanged(object? sender, System.EventArgs e)
-        {
-            CultureInfo currentLanguage = App.Language;
-
-            foreach(MenuItem item in lang.Items)
-            {
-                CultureInfo ci = item.Tag as CultureInfo;
-                item.IsChecked = ci != null && ci.Equals(currentLanguage);
-            }
         }
     }
 }
