86b04890dd2fe22ff34b0a10e4ab63b25f860e96
dunno <dunnotoru@yandex.ru>
2024-02-12 19:55:11 +0700

    update: avalonia editor
    AvaloniaEdit has already implemented features such as line numeration, text highlighting and the most important it works much more faster than regular textbox.
    
    to bind field i use behavior but i guess i should use filestream instead of binding to a property

---
+++ IDE/Helper/AvalonEditBehavior.cs
@@ -0,0 +1,67 @@
+﻿using ICSharpCode.AvalonEdit;
+using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System;
+
+namespace IDE.Helper
+{
+    public sealed class AvalonEditBehavior : Behavior<TextEditor>
+    {
+        public static readonly DependencyProperty GiveMeTheTextProperty =
+            DependencyProperty.Register("Content", typeof(string), typeof(AvalonEditBehavior),
+            new FrameworkPropertyMetadata(default(string), FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, PropertyChangedCallback));
+
+        public string Content
+        {
+            get { return (string)GetValue(GiveMeTheTextProperty); }
+            set { SetValue(GiveMeTheTextProperty, value); }
+        }
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            if (AssociatedObject != null)
+                AssociatedObject.TextChanged += AssociatedObjectOnTextChanged;
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            if (AssociatedObject != null)
+                AssociatedObject.TextChanged -= AssociatedObjectOnTextChanged;
+        }
+
+        private void AssociatedObjectOnTextChanged(object sender, EventArgs eventArgs)
+        {
+            var textEditor = sender as TextEditor;
+            if (textEditor != null)
+            {
+                if (textEditor.Document != null)
+                    Content = textEditor.Document.Text;
+            }
+        }
+
+        private static void PropertyChangedCallback(
+            DependencyObject dependencyObject,
+            DependencyPropertyChangedEventArgs dependencyPropertyChangedEventArgs)
+        {
+            var behavior = dependencyObject as AvalonEditBehavior;
+            if (behavior.AssociatedObject != null)
+            {
+                var editor = behavior.AssociatedObject as TextEditor;
+                if (editor.Document != null)
+                {
+                    var caretOffset = editor.CaretOffset;
+                    editor.Document.Text = dependencyPropertyChangedEventArgs.NewValue.ToString();
+                    if(editor.Document.Text.Length < caretOffset)
+                        editor.CaretOffset = 0;
+                    else
+                        editor.CaretOffset = caretOffset;
+
+
+                }
+            }
+        }
+    }
+}
+
